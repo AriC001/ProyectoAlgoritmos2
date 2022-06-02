c1557b2d64a478e1ee312826b21d28414e955f32
adrianmzb <adrianmzb@hotmail.com>
2022-06-02 01:43:00 -0300

    test

--- files/test.py
+++ files/test.py
@@ -3,13 +3,12 @@ import dictionary as dict
 
 shipDictionary = dict.Dictionary(64)
 
-print(len(shipDictionary))
-print(shipDictionary.prime)
 for i in range(64):
     shipDictionary.insert(obj.Ship("barco"+str(i), 0, 0, "N"))
 
 print("==========================")
+for i in range(len(shipDictionary)):
+    print(shipDictionary.data[i])
 
 print(shipDictionary.search(obj.getKey("barco64")))
-print(shipDictionary.search(obj.getKey("barco1")))for i in range(len(shipDictionary)):
-    print(shipDictionary.data[i])
+print(shipDictionary.search(obj.getKey("barco1")))
