794fd13c275487cfe5d22eea184e3e44ac5ab50b
AriC001 <87084361+AriC001@users.noreply.github.com>
2022-06-16 13:32:23 -0300

    serialization working on hash dictionary

--- files/test.py
+++ files/test.py
@@ -5,8 +5,8 @@ import navigation_system as ns
 
 dictionary = ns.create()
 
-index = dictionary.search(obj.getKey("barco27"),"barco27")
-print(index)
-if index:
-    print(dictionary.data[index])
-    print(ns.search(dictionary, "barco27", "15/xx"))
+#index = dictionary.search(obj.getKey("barco27"),"barco27")
+#print(index)
+#if index:
+    #print(dictionary.data[index])
+    #print(ns.search(dictionary, "barco27", "15/xx"))
